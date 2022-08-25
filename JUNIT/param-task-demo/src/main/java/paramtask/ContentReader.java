520dd1e6170e0a7996e3ded92b14aa3d63581cd5
HopeMashal <mashal.hope@gmail.com>
2022-08-25 16:04:57 +0300

    edit params demo folder

---
+++ JUNIT/param-task-demo/src/main/java/paramtask/ContentReader.java
@@ -0,0 +1,27 @@
+package paramtask;
+
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.opencsv.CSVReader;
+
+public class ContentReader {
+
+    public List<String[]> readAllLines(String filePath) throws Exception {
+      FileReader filereader = new FileReader(filePath);
+      CSVReader reader = new CSVReader(filereader);
+  
+      List<String[]> data = new ArrayList<String[]>();
+  
+      // read line by line
+      String[] record = null;
+  
+      while ((record = reader.readNext()) != null) {
+        data.add(record);
+        
+      }		
+      reader.close();
+      return data;
+    }
+}
---
+++ JUNIT/param-task-demo/src/test/java/paramtask/ParamsTask.java
@@ -0,0 +1,40 @@
+package paramtask;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class ParamsTask {
+
+  @Parameterized.Parameter(0)
+  public String firstNum;
+  @Parameterized.Parameter(1)
+  public String secondNum;
+  @Parameterized.Parameter(2)
+  public String thirdNum;
+    
+	@Parameters
+	public static Collection<String[]> input() throws Exception{
+		String path = "./src/files/ParamTask.csv";
+    ContentReader contentReader = new ContentReader();
+		List<String[]> lines = contentReader.readAllLines(path);
+    lines.remove(0);
+		return lines;
+	}
+	
+	@Test
+	@Parameters
+	public void testParam() {
+		int fNum = Integer.parseInt(firstNum);
+    int sNum = Integer.parseInt(secondNum);
+    int tNum = Integer.parseInt(thirdNum);
+    Assert.assertEquals(Math.max(fNum, sNum), tNum);
+	}
+	
+}
