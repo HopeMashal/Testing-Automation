569409b1a4470264d6f779ff85867a06b85e6d31
HopeMashal <mashal.hope@gmail.com>
2022-08-25 15:38:11 +0300

    edit rest assured demo folder

---
+++ JUNIT/rest-assured-demo/src/main/java/restassuredtask/ContentReader.java
@@ -0,0 +1,38 @@
+package restassuredtask;
+
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+
+public class ContentReader {
+
+	public static List<ArrayList<String>> readFileCSV = new ArrayList<ArrayList<String>>();
+
+		public List<ArrayList<String>> readDataLineByLine(String file){
+      try {
+        FileReader filereader = new FileReader(file);
+        CSVReader csvReader = new CSVReaderBuilder(filereader)
+                                  .withSkipLines(1)
+                                  .build();
+        List<String[]> allData = csvReader.readAll();
+        ArrayList<String> myList;
+        for (String[] row : allData) { 
+          myList = new ArrayList<String>();
+          for (String cell : row) {
+            myList.add(cell);
+          }
+          readFileCSV.add(myList);
+        }
+        csvReader.close();
+        filereader.close();
+      }
+      catch (Exception e) {
+        e.printStackTrace();
+      }
+	    return readFileCSV;
+	  }
+
+}
---
+++ JUNIT/rest-assured-demo/src/test/java/restassuredtask/DriverTest.java
@@ -0,0 +1,52 @@
+package restassuredtask;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+public class DriverTest {
+  static String readPath = "./src/files/Tasks.csv";
+  static String failurePath = "./src/files/Failure.csv";
+  static ContentReader reader;
+  static ContentWriter writer;
+  static List<String[]> FailureList;
+  
+  @BeforeClass
+  public static void beforeClass(){
+		reader = new ContentReader();
+    writer = new ContentWriter();
+    FailureList = new ArrayList<String[]>();
+  }
+
+  @Test
+  public void TestCaseOne(){
+    List<ArrayList<String>> lines = reader.readDataLineByLine(readPath);
+    for (ArrayList<String> line : lines) {
+			Response response = RestAssured.get("https://jsonplaceholder.typicode.com" + line.get(0));
+			if(Integer.parseInt(line.get(1)) != response.getStatusCode()){
+        String[] myString = new String[]{line.get(0),line.get(1),String.valueOf(response.getStatusCode())};
+        FailureList.add(myString);
+      }
+		}
+    Assert.assertEquals(FailureList.size(), 0);
+  }
+
+  @After 
+  public void after(){
+    System.out.println("After Test");
+  }
+
+  @AfterClass
+  public static void afterClass(){
+    String[] myStr = new String[]{"Route","Expected Status","Actual Status"};
+    writer.writeDataLineByLine(failurePath, FailureList, myStr);
+  }
+}
