0d63899dc41c6a6ed179ef1de88ca61b39ff0d09
HopeMashal <mashal.hope@gmail.com>
2022-09-10 21:45:58 +0300

    add selenium

---
+++ Selenium/selenium-task/src/test/java/OpenBrowserTest.java
@@ -0,0 +1,21 @@
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class OpenBrowserTest {
+	@Parameters({"Browser"})
+    @Test
+    public void checkBrowser(String Browser) throws InterruptedException{
+		WebDriver driver = OpenBrowser.openBrowser(Browser);
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com/");
+		Thread.sleep(5000);
+		driver.get("https://www.bing.com/");
+		Thread.sleep(5000);
+		driver.navigate().back();
+		Thread.sleep(5000);
+		driver.navigate().forward();
+		Thread.sleep(5000);
+		driver.quit();
+    }
+}
