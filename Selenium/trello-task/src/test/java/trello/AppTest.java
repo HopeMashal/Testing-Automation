a9394e18cede493b3ebb08710c4e86485bb56c02
HopeMashal <mashal.hope@gmail.com>
2022-09-24 12:05:03 +0300

    edit few files

--- Selenium/trello-task/src/test/java/trello/AppTest.java
+++
@@ -1,6 +0,0 @@
-package trello;
-
-public class AppTest 
-{
-    
-}
---
+++ Selenium/trello-task/src/test/java/trello/TrelloTask.java
@@ -0,0 +1,79 @@
+package trello;
+
+import java.util.List;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import trello.Core.OpenBrowser;
+import trello.Pages.LoginEmail;
+import trello.Pages.LoginPassword;
+
+public class TrelloTask {
+  static String Browser;
+  static String CSVPath;
+  static WebDriver driver;
+  static String URL = "https://trello.com";
+  
+  @Parameters({"Browser","CSVPath"})
+  @BeforeSuite
+  public void beforeSuite(String Browser,String CSVPath) throws InterruptedException{
+    TrelloTask.Browser = Browser;
+    TrelloTask.CSVPath = CSVPath;
+    driver = OpenBrowser.openBrowser(TrelloTask.Browser);
+    driver.manage().window().maximize();
+    driver.get(URL+"/login");
+    LoginEmail loginEmail = new LoginEmail(driver);
+    loginEmail.Continue("mashal.hope@gmail.com");
+    Thread.sleep(2000);
+    LoginPassword loginPassword = new LoginPassword(driver);
+    loginPassword.Login("**");
+  }
+
+  /* @DataProvider
+  public static Object[][] createTask() throws Exception{
+    List<String[]> lines = CSVFile.readAllLines(CreateElementTask.CSVPath);
+    lines.remove(0);
+    Object[][] data = new Object[lines.size()][lines.get(0).length];
+    int index = 0;
+    for(String[] line: lines){
+      data[index] = line;
+      index++;
+    }
+    return data;
+  }
+
+  @Test(dataProvider = "createTask")
+  public void testCreateTask(String firstName,String lastName, String address1, String address2, String city, String state, String zip, String country, String birthday, String color, String age, String website, String phone, String common, String note) throws InterruptedException{
+    HomePage homePage = new HomePage(driver);
+    homePage.AddressClick();
+    Thread.sleep(2000);
+    AddressPage addressPage = new AddressPage(driver);
+    int addNum1 = addressPage.AddressCounter();
+    addressPage.newAddressClick();
+    Thread.sleep(2000);
+    NewAddress newAddress = new NewAddress(driver);
+    newAddress.FillData(firstName, lastName, address1, address2, city, state, zip, country, birthday, color, age, website, phone, common, note);
+    Thread.sleep(2000);
+    CheckData checkData = new CheckData(driver);
+    checkData.CheckAddressData(firstName, lastName, address1, address2, city, state, zip, country, birthday, color, age, website, phone, common, note);
+    Thread.sleep(2000);
+    checkData.ListClick();
+    Thread.sleep(2000);
+    AddressPage addressPage2 = new AddressPage(driver);
+    int addNum2 = addressPage2.AddressCounter();
+    Assert.assertEquals(addNum2, (addNum1+1), "Address Numbers NOT Match!!");
+  }
+    
+  @AfterSuite
+  public void afterSuite() {
+    HomePage homePage = new HomePage(driver);
+    homePage.Logout();
+    driver.quit();
+  } */
+}
