9bdd497faef88a6ccf26660999cb7ea9ed7440b0
HopeMashal <mashal.hope@gmail.com>
2022-10-01 12:05:14 +0300

    edit allure task

--- Selenium/allure-task/src/main/java/alluretask/Core/OpenBrowser.java
+++ Selenium/allure-task/src/main/java/alluretask/Core/OpenBrowser.java
@@ -51,7 +51,7 @@ public class OpenBrowser {
 		ChromeOptions options = new ChromeOptions();
 		options.setExperimentalOption("prefs", chromePrefs);
 		options.addArguments("--start-maximized");
-		//options.addArguments("--headless");
+		options.addArguments("--headless");
 		options.addArguments("--disable-infobars");
 
 		driver = new ChromeDriver(options);
